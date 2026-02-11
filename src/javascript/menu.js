e7bbb635107f7bf60438d2c3dad49eaa514493c0
Sofia <song0002@stud.ek.dk>
2026-02-11 16:32:30 +0100

    jeg

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -1,9 +1,9 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
 
-burger.addEventListener("click", burgerClick);
-
-function burgerClick() {
-  burger.classList.toggle("active");
-  nav.classList.toggle("active");
+if (burger && nav) {
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+  });
 }
