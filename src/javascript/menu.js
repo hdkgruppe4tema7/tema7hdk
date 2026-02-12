fe4ce3cf786c6915f7d847e5511fdd54da52be7c
Ophelie <opbe0001@tud.ek.dk>
2026-02-12 10:34:03 +0100

    tilføjer billeder og ændringer i header

--- src/javascript/menu.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector(".burger");
-const nav = document.querySelector("nav");
-
-if (burger && nav) {
-  burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    nav.classList.toggle("active");
-  });
-}
